39f533f9b56e2c8e2230735cc4a84f2e1e5e04a1
andreas50 <andreas@eckner.com>
2015-08-11 15:27:33 -0400

    Move dataset preparation into new file

---
+++ R/dataset_preparation.R
@@ -0,0 +1,9 @@
+# Lynx data
+if (0) {
+  lynx_1942 <- read.csv("data/lynx_1942.csv", check.names=FALSE)
+  # NEXT: convert to "uts_vector" before saving
+  save(lynx_1942, file="data/lynx_1942.rda")
+}
+
+
+# Grapes data
