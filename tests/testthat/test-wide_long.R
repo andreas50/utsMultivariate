081ee5d44df76e7e5a7aa823be4f449e4d719de2
Andreas Eckner <andreas@eckner.com>
2016-02-02 12:48:18 -0500

    Add uts_vector_long() tests

--- tests/testthat/test-wide_long.R
+++ tests/testthat/test-wide_long.R
@@ -5,11 +5,25 @@ test_that("uts_vector_wide works",{
   expect_error(uts_vector_wide(1, Sys.time()))
   expect_error(uts_vector_wide(matrix(1:4, 2, 2), Sys.time()))
   expect_error(uts_vector_wide(matrix(1:4, 2, 2), Sys.time() + ddays(1:2), names="a"))
+  expect_error(uts_vector_wide(matrix(1:4, 2, 2), 1:2))
   
   # Regression tests
   data <- data.frame(apples=1:10, oranges=letters[1:10], bananas=month.name[1:10])
   expect_equal_to_reference(
     start(uts_vector_wide(data, times=as.POSIXct("2015-01-01") + ddays(1:10))),
-    file="test-wide.rds"
+    file="test-uts_vector_wide.rds"
+  )
+})
+
+
+test_that("uts_vector_long works",{
+  # Argument checking
+  expect_error(uts_vector_long(1:2, Sys.time(), "a"))
+  expect_error(uts_vector_long(1, Sys.time(), c("a", "b")))
+  
+  # Regression tests
+  expect_equal_to_reference(
+    uts_vector_long(values=1:10, times=as.POSIXct("2010-01-01") + days(1:10), names=rep(c("a", "b", "c"), length=10)),
+    file="test-uts_vector_long.rds"
   )
 })
