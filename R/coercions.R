059ca0de6e262f17eb190352c71fcb201d8b3e95
Andreas Eckner <andreas@eckner.com>
2016-02-29 14:58:57 -0500

    Add coercion from zoo

--- R/coercions.R
+++ R/coercions.R
@@ -21,6 +21,13 @@
 #' utsv[[1]]
 #' utsv$oranges
 #' 
+#' # Convert a 3-dimensional "zoo"
+#' if (requireNamespace("zoo", quietly = TRUE)) {
+#'   zoo1 <- zoo::zoo(matrix(1:12, 4, 3), as.Date("2003-01-01") + 0:3)
+#'   utsv <- as.uts_vector(zoo1)
+#'   utsv[[1]]
+#' }
+#' 
 #' @seealso \code{\link[uts:as.uts]{as.uts}} (in package \code{uts}) for converting just univariate time series.
 as.uts_vector <- function(x, ...) UseMethod("as.uts_vector")
 
@@ -38,4 +45,17 @@ as.uts_vector.ts <- function(x, ...)
     times <- floor_date(times + days(5), unit="month")
   uts_vector_wide(values, times)
 }
+
+
+#' @describeIn as.uts_vector convert a \code{\link[zoo:zoo]{zoo}} object
+as.uts_vector.zoo <- function(x, ...)
+{
+  times <- as.POSIXct(as.character(attr(x, "index")))
+  values <- as.matrix(x)
+  colnames(values) <- colnames(x)
+  
+  uts_vector_wide(x, times)
+}
+
+
 
--- tests/testthat/test-coercions.R
+++ tests/testthat/test-coercions.R
@@ -4,4 +4,37 @@ test_that("coercions work",{
   # ts
   ts1 <- ts(matrix(1:20, 10, 2), start=c(2016, 1), frequency=12, names=c("apples", "oranges"))
   expect_equal_to_reference(as.uts_vector(ts1), file="test-coercions_ts.rds")
+  
+# fts
+  if (requireNamespace("fts", quietly = TRUE)) {
+  
+  }
+  
+  # irts
+  if (requireNamespace("tseries", quietly = TRUE)) {
+
+  }
+
+  # its
+  if (requireNamespace("its", quietly = TRUE)) {
+
+  }
+  
+  # xts
+  if (requireNamespace("xts", quietly = TRUE)) {
+
+  }
+  
+  # zoo
+  if (requireNamespace("zoo", quietly = TRUE)) {
+    # without names
+    values <- matrix(1:12, 4, 3)
+    zoo1 <- zoo::zoo(values, as.Date("2003-01-01") + 0:3)
+    expect_equal_to_reference(as.uts_vector(zoo1), file="test-coercions_zoo_1.rds")
+    
+    # with names
+    colnames(values) <- c("a", "b", "c")
+    zoo2 <- zoo::zoo(values, as.Date("2003-01-01") + 0:3)
+    expect_equal_to_reference(as.uts_vector(zoo2), file="test-coercions_zoo_2.rds")
+  }
 })
