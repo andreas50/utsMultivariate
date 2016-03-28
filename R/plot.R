c41eea63c9d04c766a98d8e80fec9d32073bb91a
Andreas Eckner <andreas@eckner.com>
2016-03-28 15:03:28 -0400

    Finished basic plot_single_uts_vector

--- R/plot.R
+++ R/plot.R
@@ -15,16 +15,16 @@
 #' 
 #' @seealso \code{\link{plot.uts}}
 #' @examples
-#' # plot all time series at once
+#' # Plot all time series at once
 #' old <- par(mfrow=c(1, 2))
 #' plot(ex_uts_vector())
 #' plot(ex_uts_vector(), max_dt=dhours(12), type="b")   # don't connect points more than 12 hours apart
 #' par(old)
 #' 
-#' # plot one time series at a time
+#' # Plot one time series at a time
 #' plot(ex_uts_vector(), ask=TRUE)
 #' 
-#' # plot multiple time series in a single plot by superimposing the plots
+#' # Plot multiple time series in a single plot by superimposing the plots
 #' plot(ex_uts_vector(), plot.type="single")
 plot.uts_vector <- function(x, ..., plot.type="multiple", ask=getOption("device.ask.default"),
   legend=TRUE, legend.x="topright", legend.y=NULL)
@@ -74,9 +74,31 @@ plot.uts_vector <- function(x, ..., plot.type="multiple", ask=getOption("device.
 #' plot_single_uts_vector(ex_uts_vector())
 plot_single_uts_vector <- function(x, ..., xlab="", ylab="", legend=TRUE, legend.x="topright", legend.y=NULL)
 {
+  # Remove time series with zero observations
+  len <- sapply(x, length)
+  if (0) {
+    #x <- x[len > 0]    # not implemented yet
+  } else {
+    for (l in rev(len))
+      x[[l]] <- NULL
+  }
+  if (length(x) == 0)
+    stop("Nothing to plot")
+  
   # Set up empty plotting canvas
   tmp_x <- c(min(start(x)), max(end(x)))
   tmp_y <- range(range(x))
   plot(tmp_x, tmp_y, type="n", xlab=xlab, ylab=ylab, ...)
+  
+  # Recycle arguments
+  args <- list(...)
+  
+  # Plot the individual time series
+  for (j in seq_along(x)) {
+    #color <- col_tab[(j-1) %% length(col_tab) + 1]
+    #lty <- lty_tab[j]
+    #lines(segments[[j]]$x, segments[[j]]$y, type=type, col=color, lty=lty, lwd=lwd[i], ...)
+    plot(x[[j]], plot.new=FALSE)
+  }
 }
 
